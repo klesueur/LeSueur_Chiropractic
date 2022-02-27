98044622cf761363c33c0201769740e6317e9944
Kara LeSueur <klesueur@kornerstonecredit.com>
2022-02-26 19:56:53 -0800

    NavBar showing in all views

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.scss';
 import routes from './routes';
+import NavBar from './components/NavBar/NavBar';
 
 
 function App() {
 
   return (
     <div className="App">
+      <NavBar />
       { routes }
     </div>
   );
